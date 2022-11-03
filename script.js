47f948bcba12da6985e3e1f6736eaff15850312e
BondT1 <tombond248@gmail.com>
2022-11-03 17:13:44 +0000

    started fetching API data

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-var openWeatherApiKey = 'f4ec5dcff4823d5712d2cbe8b9348d8c';
-var openWeatherCoordinatesUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var ApiKey = 'f4ec5dcff4823d5712d2cbe8b9348d8c';
+var openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='
-var userFormEL = $('#search-city');
+var coordinates = openWeatherUrl + city + '$appid' + ApiKey
+var FormEL = $('#search-city');
 var forecastEl = $('.forecast');
-var cityInputEl = $('#city');
-var fiveDayEl = $('#five-forecast');
-var searchHistoryEl = $('#history');
+var cityEl = $('#city');
+var fiveForecastEl = $('#five-forecast');
+var HistoryEl = $('#history');
 var currentDay = moment().format('M/DD/YYYY');
-const weatherIconUrl = 'http://openweathermap.org/img/wn/';
 var searchHistoryArray = loadSearchHistory();
 
+
+// Fetch weather data from API url
+
+function fetchWeather() {
+    fetch (coordinates) {
+        then (function (coordinateResult) {
+            if (coordinateResult.ok) {
+                coordinateResult.json().then(function (data) {
+                    var lon = data.coord.lon;
+                    var lat = data.coord.lon;
+                    var Apifetch = oneCallUrl + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=imperial';
+                }
+            }
+        }
+    }
+
+}
 
 
