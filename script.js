3907855fba6624a811623ce02a9690174044d273
BondT1 <tombond248@gmail.com>
2022-11-04 18:08:35 +0000

    further work on current weather and local storage

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
 var ApiKey = 'f4ec5dcff4823d5712d2cbe8b9348d8c';
 var openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='
 var coordinates = openWeatherUrl + city + '$appid' + ApiKey
-var FormEL = $('#search-city');
+var formEL = $('#search-city');
+var currentDate = moment().format('M/DD/YYYY');
 var forecastEl = $('.forecast');
+var historyArray = loadSearchHistory();
+var historyEl = $('#history');
 var cityEl = $('#city');
 var fiveForecastEl = $('#five-forecast');
-var HistoryEl = $('#history');
-var currentDay = moment().format('M/DD/YYYY');
-var searchHistoryArray = loadSearchHistory();
+var weatherIcon = 'http://openweathermap.org/img/wn/';
 
 
 // Fetch weather data from API url
@@ -20,12 +31,75 @@ function fetchWeather() {
                 coordinateResult.json().then(function (data) {
                     var lon = data.coord.lon;
                     var lat = data.coord.lon;
-                    var Apifetch = oneCallUrl + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=imperial';
+                    var apiFetch = oneCallUrl + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=imperial';
+
+                    fetch(apiFetch)
+                        .then(function (weatherResult) {
+                            if (weatherResult.ok) {
+                                weatherResult.json().then(function (weatherInfo) {
+
+                                    // current day
+                                    var weatherNow = $('<div></div>')
+                                    .attr({ id: weather-now})
+
+                                    var icon = weatherResult.current.weather[0].Icon
+                                    var cityIcon = weatherIcon + icon + '.png';
+                                    var iconEL = $('<img><img>')
+                                        .attr({ id: 'weather-current-icon',
+                                                src: cityIcon,
+                                                alt: 'Icon for weather'});
+
+                                    var currentHeading = $('<h2></h2>')
+                                        .text(city + ' (' + currentDate + ')');
+
+                                    var weatherListCurrent = $('<ul></ul>');
+
+                                    var weatherDetailsCurrent = 
+
+                                    // need to continue with this 
+
+                                    
+
+
+
+
+                                })
+                            }
+                        })
                 }
             }
         }
     }
 
 }
+
+// local storage
+
+function saveLocalStorage() {
+    localStorage.setItem('history', JSON.stringify(HistoryArray));    
+}
+
+function insertLocalStorage() {
+    var historyArray = JSON.parse(localStorage.getItem('history'));
+
+    // citySearch: []
+
+    for (var i = 0; i < historyArray.citySearch.length; i++) {
+        history(historyArray.citySearch[i]);
+    }
+
+    return historyArray;
+}
+
+function history(city) {
+    
+}
+
+
+
+
+// $('#search-button').on('click', function () {
+
+// })
 
 
